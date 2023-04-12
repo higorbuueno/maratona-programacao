873a6ff2c1171675c0972a8a17329664bc766b75
higorbuueno <buenoshigor@gmail.com>
2023-04-11 22:49:26 -0300

    Salvando alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,19 @@ export class AppComponent {
   cifraVigenere: string = '';
   cercaTrilho: string = '';
 
+  chaveCesar: string = '';
+  chaveVigenere: string = '';
+  chaveTrilho: string = '';
+
   resultadoCesar: string = '';
   resultadoVigenere: string = '';
   resultadoTrilho: string = '';
 
+  cesarFocus: boolean = false;
+  vigenereFocus: boolean = false;
+  trilhoFocus: boolean = false;
 
-  onClickCifraCesar(){
+  onChangeCifraCesar(){
     // TODO: FAZER A FUNÇÃO E DEFINIR A VARIÁVEL resultadoCesar com o codificado.
     this.resultadoCesar = this.cifraCesar
   }
